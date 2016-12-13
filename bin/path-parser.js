26152a8157b1a10d43909d368b99b89a5e0a8738
James Speirs <james.speirs@gmail.com>
2016-12-13 15:41:38 -0700

    path parser working

--- bin/path-parser.js
+++ bin/path-parser.js
@@ -15,19 +15,79 @@
  *    limitations under the License.
  **/
 'use strict';
+const rxColon = /^:([_$a-z][_$a-z0-9]*)(\*|\?|\*\?)?$/;
+const rxHandlebar = /^{([_$a-z][_$a-z0-9]*)(\*|\?|\*\?)?}$/;
+const rxDoubleHandlebar = /^{{([_$a-z][_$a-z0-9]*)(\*|\?|\*\?)?}}$/;
 
-exports.colon = function(path) {
-    const rx = /:([_$a-z][_$a-z0-9]*)/ig;
+/**
+ * Get a function that can be called to provide parameter matches for url paths.
+ * @param {string} path The matching path descriptor.
+ * @param {string} [format]
+ * @returns {Function}
+ */
+exports.parser = function (path, format) {
+    const params = [];
+    const rx = typeof path === 'string'
+        ? exports.createRxFromStringPath(path, format, params)
+        : path;
 
-    let match;
-    while (match = rx.exec(path)) {
-        console.log(match);
+    // validate that rx could be acquired
+    if (!(rx instanceof RegExp)) {
+        const err = Error('Path definition must be a string or a regular expression.');
+        err.code = 'ESSRPTH';
+        throw err;
+    }
+
+    /**
+     * Take a URL path and determine the path parameters.
+     * @param {string} urlPath
+     * @returns {Object}
+     */
+    return function(urlPath) {
+        const match = rx.exec(urlPath);
+        if (!match) return null;
+
+        const parameters = {};
+        for (let i = 1; i < match.length; i++) {
+            if (match[i] !== undefined) parameters[params[i - 1][1]] = match[i];
+        }
+
+        return parameters;
     }
 };
 
-console.log(exports.colon('/foo/:id/bar/:xyz'));
+/**
+ * Create a regular expression from a string path definition.
+ * @param {string} path
+ * @param {string} [format='colon']
+ * @param {Array} [params]
+ * @returns {RegExp}
+ */
+exports.createRxFromStringPath = function (path, format, params) {
+    if (!format) format = 'colon';
+    path = path.replace(/^\//, '').replace(/\/$/, '');
+    const str = path.split('/')
+        .map(function(item, index) {
+            const prefix = (index ? '\\/' : '');
+            const rx = format === 'colon'
+                ? rxColon
+                : format === 'handlebar'
+                ? rxHandlebar
+                : rxDoubleHandlebar;
 
-/*
-colon: injectorReplacement(function() { return /:([_$a-z][_$a-z0-9]*)/ig }),
-    doubleHandlebar: injectorReplacement(function() { return /{{([_$a-z][_$a-z0-9]*)}}/ig }),
-    handlebar: injectorReplacement(function() { return /{([_$a-z][_$a-z0-9]*)}/ig })*/
+            const match = rx.exec(item);
+            if (match || item === '*') {
+                const modifier = match ? match[2] : '*';
+                if (params) params.push(match);
+                if (!modifier || modifier === '*') {
+                    return prefix + (modifier ? '([\\s\\S]+?)' : '([^\\/]+)');
+                } else {
+                    return '(?:' + prefix + (modifier === '?' ? '([^\\/]+)' : '([\\s\\S]+?)') + ')?';
+                }
+            } else {
+                return prefix + item;
+            }
+        })
+        .join('');
+    return new RegExp('^' + str + '$');
+};
---
+++ test/path-parser.js
@@ -0,0 +1,266 @@
+/**
+ *  @license
+ *    Copyright 2016 Brigham Young University
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ **/
+'use strict';
+const expect                = require('chai').expect;
+const parser                = require('../bin/path-parser');
+
+describe('path-parser', () => {
+
+    describe('#createRxFromStringPath', () => {
+
+        it('foo', () => {
+            const rx = parser.createRxFromStringPath('foo', 'colon');
+            expect(rx.toString()).to.equal('/^foo$/');
+        });
+
+        it('foo/', () => {
+            const rx = parser.createRxFromStringPath('foo/', 'colon');
+            expect(rx.toString()).to.equal('/^foo$/');
+        });
+
+        it('/foo/', () => {
+            const rx = parser.createRxFromStringPath('/foo/', 'colon');
+            expect(rx.toString()).to.equal('/^foo$/');
+        });
+
+        it('/:foo', () => {
+            const params = [];
+            const rx = parser.createRxFromStringPath('/:foo', 'colon', params);
+            expect(rx.toString()).to.equal('/^([^\\/]+)$/');
+        });
+
+        it('/:foo*', () => {
+            const params = [];
+            const rx = parser.createRxFromStringPath('/:foo*', 'colon', params);
+            expect(rx.toString()).to.equal('/^([\\s\\S]+?)$/');
+        });
+
+        it('/:foo?', () => {
+            const params = [];
+            const rx = parser.createRxFromStringPath('/:foo?', 'colon', params);
+            expect(rx.toString()).to.equal('/^(?:([^\\/]+))?$/');
+        });
+
+        it('/:foo*?', () => {
+            const params = [];
+            const rx = parser.createRxFromStringPath('/:foo*?', 'colon', params);
+            expect(rx.toString()).to.equal('/^(?:([\\s\\S]+?))?$/');
+        });
+
+        it('/{foo}', () => {
+            const params = [];
+            const rx = parser.createRxFromStringPath('/{foo}', 'handlebar', params);
+            expect(rx.toString()).to.equal('/^([^\\/]+)$/');
+        });
+
+        it('/{{foo}}', () => {
+            const params = [];
+            const rx = parser.createRxFromStringPath('/{{foo}}', 'doubleHandlebar', params);
+            expect(rx.toString()).to.equal('/^([^\\/]+)$/');
+        });
+
+    });
+
+    describe('parser', () => {
+
+        describe('foo', () => {
+            const fn = parser.parser('foo', 'colon');
+
+            it('foo', () => {
+                expect(fn('foo')).to.deep.equal({});
+            });
+
+            it('bar', () => {
+                expect(fn('bar')).to.deep.equal(null);
+            });
+        });
+
+        describe(':foo', () => {
+            const fn = parser.parser(':foo', 'colon');
+
+            it('foo', () => {
+                expect(fn('foo')).to.deep.equal({ foo: 'foo' });
+            });
+
+            it('bar', () => {
+                expect(fn('bar')).to.deep.equal({ foo: 'bar' });
+            });
+        });
+
+        describe('foo/:bar', () => {
+            const fn = parser.parser('foo/:bar', 'colon');
+
+            it('foo', () => {
+                expect(fn('foo')).to.deep.equal(null);
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal({ bar: 'bar' });
+            });
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal(null);
+            });
+        });
+
+        describe('foo/:bar/:baz', () => {
+            const fn = parser.parser('foo/:bar/:baz', 'colon');
+
+            it('foo', () => {
+                expect(fn('foo')).to.equal(null);
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal(null);
+            });
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal({ bar: 'bar', baz: 'baz' });
+            });
+        });
+
+        describe('foo/:bar/baz', () => {
+            const fn = parser.parser('foo/:bar/baz', 'colon');
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal({ bar: 'bar' });
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal(null);
+            });
+        });
+
+        describe('foo/:bar*', () => {
+            const fn = parser.parser('foo/:bar*', 'colon');
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal({ bar: 'bar/baz' });
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal({ bar: 'bar' });
+            });
+
+            it('foo', () => {
+                expect(fn('foo')).to.deep.equal(null);
+            });
+        });
+
+        describe('foo/:bar?', () => {
+            const fn = parser.parser('foo/:bar?', 'colon');
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal(null);
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal({ bar: 'bar' });
+            });
+
+            it('foo', () => {
+                expect(fn('foo')).to.deep.equal({});
+            });
+        });
+
+        describe('foo/:bar?/:baz', () => {
+            const fn = parser.parser('foo/:bar?/:baz', 'colon');
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal({ bar: 'bar', baz: 'baz' });
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal({ baz: 'bar' });
+            });
+
+            it('foo', () => {
+                expect(fn('foo')).to.deep.equal(null);
+            });
+        });
+
+        describe('foo/:bar?/baz', () => {
+            const fn = parser.parser('foo/:bar?/baz', 'colon');
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal({ bar: 'bar' });
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal(null);
+            });
+
+            it('foo/baz', () => {
+                expect(fn('foo/baz')).to.deep.equal({});
+            });
+        });
+
+        describe('foo/:bar?/baz/:abc', () => {
+            const fn = parser.parser('foo/:bar?/baz/:abc', 'colon');
+
+            it('foo/bar/baz/abc', () => {
+                expect(fn('foo/bar/baz/abc')).to.deep.equal({ abc: 'abc', bar: 'bar' });
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal(null);
+            });
+
+            it('foo/baz/abc', () => {
+                expect(fn('foo/baz/abc')).to.deep.equal({ abc: 'abc' });
+            });
+        });
+
+        describe('foo/:bar*/baz', () => {
+            const fn = parser.parser('foo/:bar*/baz', 'colon');
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal({ bar: 'bar' });
+            });
+
+            it('foo/bar', () => {
+                expect(fn('foo/bar')).to.deep.equal(null);
+            });
+
+            it('foo/bar/be/que/baz', () => {
+                expect(fn('foo/bar/be/que/baz')).to.deep.equal({ bar: 'bar/be/que'});
+            });
+        });
+
+        describe('foo/:bar*/:baz*', () => {
+            const fn = parser.parser('foo/:bar*/:baz*', 'colon');
+
+            it('foo/bar/baz', () => {
+                expect(fn('foo/bar/baz')).to.deep.equal({ bar: 'bar', baz: 'baz' });
+            });
+
+            it('foo/bar/baz/abc', () => {
+                expect(fn('foo/bar/baz/abc')).to.deep.equal({ bar: 'bar', baz: 'baz/abc' });
+            });
+
+            it('foo/bar/be/que/baz', () => {
+                expect(fn('foo/bar/be/que/baz')).to.deep.equal({ bar: 'bar', baz: 'be/que/baz'});
+            });
+
+            it('foo', () => {
+                expect(fn('foo')).to.deep.equal(null);
+            });
+        });
+
+    });
+
+});
